14eac1eafb900b70484a4d98124395a3cc16bc21
Ian Whitmore <ianwhitmor@gmail.com>
2021-11-10 14:56:39 -0600

    Create IdentityUserService in services

---
+++ ECommerceApp/Services/Identity/IdentityUserService.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using ECommerceApp.Models;
+using ECommerceApp.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Logging;
+
+namespace ECommerceApp.Services.Identity
+{
+    public interface IUserService
+    {
+        Task<UserDto> Register(RegisterData data, ModelStateDictionary modelState);
+        Task<UserDto> Authenticate(LoginData data);
+        Task<UserDto> GetUser(ClaimsPrincipal user);
+    }
+
+    public class IdentityUserService : IUserService
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public IdentityUserService(UserManager<IdentityUser> userManager, ILogger<IdentityUserService> logger)
+        {
+            this.userManager = userManager;
+            Logger = logger;
+        }
+
+        public ILogger<IdentityUserService> Logger { get; }
+
+        public async Task<UserDto> Authenticate(LoginData data)
+        {
+            var user = await userManager.FindByNameAsync(data.Username);
+
+            if (await userManager.CheckPasswordAsync(user, data.Password))
+            {
+                return await CreateUserDto(user);
+            }
+
+            Logger.LogInformation("Invalid login for username '{Username}'", data.Username);
+            return null;
+        }
+
+        public async Task<UserDto> GetUser(ClaimsPrincipal principal)
+        {
+            var user = await userManager.GetUserAsync(principal);
+            if (user == null) return null;
+
+            return await CreateUserDto(user);
+        }
+
+        public async Task<UserDto> Register(RegisterData data, ModelStateDictionary modelState)
+        {
+            var user = new IdentityUser
+            {
+                Email = data.Email,
+                UserName = data.Username,
+            };
+            var result = await userManager.CreateAsync(user, data.Password);
+
+            if (result.Succeeded)
+            {
+                return await CreateUserDto(user);
+            }
+
+            foreach (var error in result.Errors)
+            {
+                var errorKey =
+                    error.Code.Contains("Password") ? nameof(data.Password) :
+                    error.Code.Contains("Email") ? nameof(data.Email) :
+                    error.Code.Contains("UserName") ? nameof(data.Username) :
+                    "";
+                modelState.AddModelError(errorKey, error.Description);
+            }
+
+            return null;
+        }
+
+        private async Task<UserDto> CreateUserDto(IdentityUser user)
+        {
+            return new UserDto
+            {
+                UserId = user.Id,
+                Email = user.Email,
+                Username = user.UserName,
+            };
+        }
+    }
+}
