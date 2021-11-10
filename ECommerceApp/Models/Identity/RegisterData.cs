91a15978d535691bcc79c10c02c381c0299e1863
Ian Whitmore <ianwhitmor@gmail.com>
2021-11-10 14:50:55 -0600

    create RegisterData.cs in models

---
+++ ECommerceApp/Models/Identity/RegisterData.cs
@@ -0,0 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceApp.Models.Identity
+{
+    public class RegisterData
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public bool AcceptedTerms { get; set; }
+    }
+}
