8bc951bc6bca12b119dae4b45d5aacb325f2ea38
Ian Whitmore <ianwhitmor@gmail.com>
2021-11-14 23:17:27 -0600

    create DatabaseProductRepository

---
+++ ECommerceApp/Services/DatabaseProductRepository.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ECommerceApp.Data;
+using ECommerceApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceApp.Services
+{
+    public class DatabaseProductRepository : IProductRepository
+    {
+        private readonly ECommerceDbContext _context;
+
+        public DatabaseProductRepository(ECommerceDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Product>> GetAll()
+        {
+            return await _context.Products.ToListAsync();
+        }
+    }
+}
